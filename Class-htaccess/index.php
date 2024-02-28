f20ec4b7cffa584e2582ff95c4154ae981dc69b2
kaipasciak <123124121+kaipasciak@users.noreply.github.com>
2024-02-28 14:33:59 -0500

    Create index.php

---
+++ Class-htaccess/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <meta charset="utf-8">
+  <title>HT ACCESS example</title>
+  <meta name="author" content="Kai Pasciak, Walter Clay, Arjun Nair">
+  <meta name="description" content="Class Exercise">
+</head>
+<body>
+  <h1>Landing page, not password protected</h1>
+</body>
+</html>

e728da2cf95ce2b93eabb95e1d07b6208e5158c6
Walter Clay <123520239+walterclay7@users.noreply.github.com>
2024-02-28 14:37:41 -0500

    Create index.php

---
+++ Class-htaccess/protected/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HT ACCESS Example</title>
+    <meta name="author" content="Walter Clay, Kai Pasciak, Arjun Nair">
+    <meta name="description" content="Class Exercise">
+</head>
+<body>
+
+<h1>password protected</h1>
+    
+</body>
+</html>
